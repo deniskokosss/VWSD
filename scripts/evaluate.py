1fd5c29b1e999efec1c747dd851c36d3675c6704
denis <deniskokosss@mail.ru>
2022-11-05 20:40:03 +0300

    moved evaluation function to separate module

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -2,20 +2,8 @@ import sys
 from pathlib import Path
 
 import pandas as pd
-import numpy as np
-from sklearn.metrics import top_k_accuracy_score
 
-
-def get_golden_index(df: pd.DataFrame, gold: pd.DataFrame) -> np.array:
-    ans = np.argwhere(df.values == gold.values)[:, 1]
-    return ans
-
-def mrr(gold: np.array, preds: np.array):
-    ranks = preds.argsort(1)[:, ::-1]
-    ans = np.argwhere(ranks == gold[:, None])
-    return (1 / (1 + ans)).mean()
-
-
+from src.utils import evaluate
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -31,10 +19,10 @@ if __name__ == '__main__':
     gold = pd.read_csv(PATH / f"{PART}.gold.v1.txt", sep='\t', header=None)
     test_idx = pd.read_csv(PATH / f"split_test.txt", sep='\t', header=None).T.values[0]
 
-    gold = get_golden_index(data.loc[test_idx, :], gold.loc[test_idx, :])
+    metrics = evaluate(data.loc[test_idx, :].values, gold.loc[test_idx, :].values, preds.values)
 
-    print(f"Accuracy@Top1 {top_k_accuracy_score(gold, preds.values, k=1):.4f}")
-    print(f"Accuracy@Top3 {top_k_accuracy_score(gold, preds.values, k=3):.4f}")
-    print(f"Mean reciprocal rank {mrr(gold, preds.values):.4f}")
+    print(f"Accuracy@Top1 {metrics['acc1']:.4f}")
+    print(f"Accuracy@Top3 {metrics['acc3']:.4f}")
+    print(f"Mean reciprocal rank {metrics['mrr']:.4f}")
 
 
